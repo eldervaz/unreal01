7a7c70ecc6e2f41bb5775dfc33811c59d423ec9b
eldervaz <eldervaz@gmail.com>
2016-05-10 13:07:29 -0500

    using namespace

--- Unrealtest01/main.cpp
+++ Unrealtest01/main.cpp
@@ -7,10 +7,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    // here
-    std::cout << "Hello, World!\n";
+    cout << "Using namespace :P more cool \n";
     return 0;
 }
