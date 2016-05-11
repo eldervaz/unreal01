a2fd2613973d410d38a99bdea1afca5811ab8bcd
eldervaz <eldervaz@gmail.com>
2016-05-10 19:13:58 -0500

    abstraction and encapsulation

--- Unrealtest01/main.cpp
+++ Unrealtest01/main.cpp
@@ -10,16 +10,35 @@
 #include <string>
 using namespace std;
 
+
+void printIntro();
+string getGuess();
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     
+    printIntro();
+    
+    string guess = getGuess();
+    
+    cout << "Your guess is " <<  guess << endl;
+    return 0;
+}
+
+string getGuess(){
+    cout << "Enter your guess: ";
+    string guess = "";
+    //cin >> guess;
+    
+    getline (cin, guess);
+    
+    return guess;
+}
+
+
+void printIntro(){
     constexpr int WORLD_LENGTH = 5;
     cout << "Using namespace "  << WORLD_LENGTH << " more cool \n";
     cout << endl;
     
-    cout << "Enter your guess: ";
-    string guess = "";
-    cin >> guess;
-    cout << "Your guess is " << guess << endl;
-    return 0;
 }
